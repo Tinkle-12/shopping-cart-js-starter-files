27c0e26fe7c98538165bc01b60343319de0a167b
Tinkle-12 <rajawattinkle6@gmail.com>
2023-02-10 23:37:10 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Stasher Reusable Storage Bag",
+    price: 45,
+    desc: "It's estimated that Americans alone use around 100 billion plastic bags every year, about 365 per person, so swapping out plastic one-time use bags for reusable silicone bags is a simple but impactful place to start when it comes to making more eco-friendly choices. ",
+    img: "images/storageBag.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "PERFECT POD Reusable Metal Coffee Pod",
+    price: 100,
+    desc: "Single-serve coffee makers definitely make mornings easier, but those plastic coffee pods are terrible for the environment.",
+    img: "images/coffeePod.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Swedish Wholesale Swedish Dish Cloths",
+    price: 25,
+    desc: "Dish cloths > paper towels. When it comes to being environmentally friendly, reusable products are the name of the game.",
+    img: "images/dishClothes.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "KitchLife Reusable Bamboo Paper Towels",
+    price: 300,
+    desc: "If you're looking for a reusable cloth that has a feel that's more similar to paper towels, then these bamboo towels are for you.",
+    img: "images/towels.jpg",
+  },
+];
